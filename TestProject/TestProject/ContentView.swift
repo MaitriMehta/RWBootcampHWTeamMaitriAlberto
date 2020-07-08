ec107903e5903962a527bfb3c8c915df717d2017
AlbertoTalavan <albertotalavan@gmail.com>
2020-07-08 18:53:37 +0200

    initial TestProject commit

---
+++ TestProject/TestProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestProject
+//
+//  Created by Alberto Talaván on 08/07/2020.
+//  Copyright © 2020 Alberto Talavan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello Maitri, Change whatever you want!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
